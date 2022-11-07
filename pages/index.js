d6d8a3290734d4f708087f1f835b7062905f8990
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2022-11-07 18:57:52 -0300

    insert banner configs

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-  const estilosDaHomePage = { //backgroundColor: "red"
+  const estilosDaHomePage = {
+    //backgroundColor: "red"
   };
 
   //   console.log(config.playlists);
@@ -17,7 +18,7 @@ function HomePage() {
       <div style={estilosDaHomePage}>
         <Menu />
         <Header />
-        <Timeline playlists={config.playlists}>Children</Timeline>
+        <Timeline playlists={config.playlists}></Timeline>
       </div>
     </>
   );
@@ -30,13 +31,33 @@ export default HomePage;
 // }
 
 const StyledHeader = styled.div`
+.banner {
+
+
+  img {
+  max-height: 100%;
+  width: 100%;
+  height: 80%;
+  border-radius: 0;
+  z-index: -1;
+  bottom: 374px;
+  position: absolute;
+  } 
+
+}
+
   img {
     width: 80px;
     height: 80px;
     border-radius: 50%;
+ 
   }
+
+
   .user-info {
     margin-top: 50px;
+    position: relative;
+    color: white;
     display: flex;
     align-items: center;
     width: 100%;
@@ -44,11 +65,23 @@ const StyledHeader = styled.div`
     gap: 16px;
   }
 `;
+
+const StyledBanner = styled.div`
+  img {
+    width: 100%;
+    height: 200px;
+  }
+`;
+
 function Header() {
   return (
     <StyledHeader>
+      <div className="banner">
+        <img
+          src={`https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1789&q=80`}
+        ></img>
+      </div>
       <section className="user-info">
-        {/* <img src="banner"></img> */}
         <img src={`https://github.com/${config.github}.png`}></img>
         <div>
           <h2>{config.name}</h2>
